e57dfcb713e580b358e36c35e002e2bc611e40d6
Michael <madetunji1376@gmail.com>
2023-05-12 14:25:42 +0100

    styling and other functionalities addedd to the app

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,50 +1,50 @@
 class DocsController < ApplicationController
-    before_action :find_doc, only: [:show, :edit, :update, :destroy]
+  before_action :find_doc, only: %i[show edit update destroy]
 
+  def index
+    @docs = Doc.where(user_id: current_user).order('created_at DESC')
+    @user = current_user
+  end
 
-    def index
-        @docs = Doc.all.order("created_at DESC")
+  def show
+    @doc = Doc.find(params[:id])
+  end
+
+  def new
+    @doc = Doc.new
+  end
+
+  def create
+    @doc = current_user.docs.new(doc_params)
+    if @doc.save
+      redirect_to docs_path
+    else
+      render 'new'
     end
+  end
 
-    def show
-        @doc = Doc.find(params[:id])
+  def edit; end
+
+  def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
     end
+  end
 
-    def new
-        @doc = Doc.new
-    end
+  def destroy
+    @doc.destroy
+    redirect_to docs_path
+  end
 
-    def create
-        @doc = current_user.docs.new(doc_params)
-        if @doc.save
-            redirect_to docs_path
-        else
-            render 'new'
-        end
-    end
+  private
 
-    def edit
-    end
+  def find_doc
+    @doc = Doc.find(params[:id])
+  end
 
-    def update
-        if @doc.update(doc_params) 
-            redirect_to @doc
-        else
-            render 'edit'
-        end
-    end
-
-    def destroy
-        @doc.destroy
-        redirect_to docs_path
-    end
-
-    private
-        def find_doc
-            @doc = Doc.find(params[:id])
-        end
-
-        def doc_params
-            params.require(:doc).permit(:title, :content)
-        end
+  def doc_params
+    params.require(:doc).permit(:title, :content)
+  end
 end
